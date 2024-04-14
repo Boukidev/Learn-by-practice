c7cf3a113f237e887bf49bce1b595cb6b87adbce
boukidev <boukidev@gmail.com>
2024-04-14 16:05:01 +0200

    Post task to JSON file

---
+++ server/api/task.post.ts
@@ -0,0 +1,18 @@
+import { readFile, writeFile } from "node:fs/promises";
+
+export default defineEventHandler(async (event) => {
+    try {
+        const { title, description } = await readBody(event);
+
+        const filePath = "server/data/tasks.json";
+        const data = await readFile(filePath, { encoding: "utf8" });
+
+        const tasks = JSON.parse(data);
+        tasks.push({ title, description, completed: false, dateCreation: new Date().toLocaleDateString() });
+        await writeFile(filePath, JSON.stringify(tasks));
+
+        return { message: "Task added successfully" };
+    } catch (error) {
+        throw new Error("Failed to write file: " + error);
+    }
+});
