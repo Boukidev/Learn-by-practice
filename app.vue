23638e9404b7e8aaf2203cffc39264a5df65f7f3
boukidev <boukidev@gmail.com>
2024-06-01 04:03:03 +0200

    init

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
-    <div>
-        <h1>Learn by practice</h1>
-    </div>
+    <h1>Auth UI</h1>
+
+    <LoginForm></LoginForm>
 </template>
+
+<script lang="ts" setup>
+    import LoginForm from "./components/form/login.vue";
+</script>
