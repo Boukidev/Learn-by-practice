17291e266dbbc2b1839f2987f6c4127d3573c4bb
boukidev <boukidev@gmail.com>
2024-06-02 17:41:50 +0200

    index components form

---
+++ components/form/index.ts
@@ -0,0 +1,5 @@
+import LoginForm from "./Login.vue";
+import RegisterForm from "./Register.vue";
+import ResetPasswordForm from "./ResetPassword.vue";
+
+export { LoginForm, RegisterForm, ResetPasswordForm };
